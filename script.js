a943af20298d648b8835e9e81cc9eb4a5418276b
arshtamboli <60126752+arshtamboli@users.noreply.github.com>
2021-05-23 12:34:02 +0530

    My portfolio website
    Created on HTML CSS & JS fully static.
    One page website.

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('dark')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'dark'){
+        document.getElementById('theme-style').href = 'dark.css'
+    }
+
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'light.css'
+    }
+
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css'
+    }
+
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
